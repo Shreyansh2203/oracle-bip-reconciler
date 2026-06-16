458748e4820b58099b70ccb8b5a716a819f1cf11
Shreyansh Srivastava <main.doga@gmail.com>
2026-06-16 13:13:07 +0530

    chore: Clean up dead code and dump files

--- test_err.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-import urllib.error
-try:
-    urllib.request.urlopen('https://urban-octo-tribble-rouge.vercel.app/docs')
-except urllib.error.HTTPError as e:
-    print(e.read().decode('utf-8'))
